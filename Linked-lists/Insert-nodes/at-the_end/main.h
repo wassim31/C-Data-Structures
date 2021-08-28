58c65653bda08a96b9f8e06d74e907f9aeca9087
wassim31 <wassimbs088@gmail.com>
2021-08-28 11:55:52 +0100

    linked lists 2

---
+++ Linked-lists/Insert-nodes/at-the_end/main.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+typedef struct node node;
+struct node
+{
+    int value;
+    node *next; 
+};
+void print(node *ptr);
+void insert_end(node **root, int x);
