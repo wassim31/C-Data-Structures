eeaf07209942c7a2310aeeefdcd522861af9f500
wassim31 <wassimbs088@gmail.com>
2021-10-08 00:14:29 +0100

    Clean Stack Implementation

--- Stack/Array-Implementation/main.c
+++ Stack/Array-Implementation/main.c
@@ -1,36 +1,20 @@
-#include "main.h"
-
-int main(int argc, char const *argv[])
+#include <stdlib.h>
+#include <stdio.h>
+int n = 0;
+typedef struct stack stack;
+struct stack{
+    int top;
+    int capacity;
+    int *array; 
+};
+stack *create(int capacity)
 {
-    printf("How many elements in the stack : ");
-    do
-    {
-        scanf("%d", &n);
-    } while(n > 10 || n <= 0);
-    int *array = malloc(n * (sizeof(int)));
-    init(array);
-    push(array, 6); // testing the overflow & maintain it 
-    print(array);
-    pop();
-    print(array);
-    free(array);
-    return 0;
-}
-void push(int *array, int x)
-{
-    top++; // 0
-    if(top == n)
-    {
-        // we copy the actual array , and put it in a new array
-        // the new_array.size = actual_array.size * 2
-        // so if the actual_array has n elements , to copy it 
-        // it took us O(n).
-        // Here we understand the benefits of linked-lists
-        int *new_array = resize(array);
-        *(array+top) = x;
-    }
-    *(array+top) = x;
-}
+    stack *new = malloc(capacity * sizeof(stack));
+    new->capacity = capacity;
+    new->top = -1;
+    new->array = malloc(sizeof(int) * capacity);
+    return new;
+};
 int *resize(int *array)
 {
     int new_size = n * 2, i = 0;
@@ -41,30 +25,55 @@ int *resize(int *array)
     }
     return new_array;
 }
-void pop()
+int push(stack *new, int number)
 {
-    if(top == -1)
+    (new->top)++;
+    if(new->top > new->capacity)
     {
-        printf("Empty Stack\n");
+        printf("Overflowed\n");
+        // we copy the actual array , and put it in a new array
+        // the new_array.size = actual_array.size * 2
+        // so if the actual_array has n elements
+        // to copy the elements , it took us O(n).
+        // Here we understand the benefits of linked-lists
+        new->array = resize(new->array);
+        *(new->array + new->top) = number;
+        return 1;
+    }
+
+    *(new->array + new->top) = number;
+    return 0;
+}
+void print(stack *new)
+{
+    for(int i = 0 ; i <= new->top ; i++)
+    {
+        printf("Element %d : %d\n", i, *(new->array+i));
+    }
+}
+void is_full(stack *new)
+{
+    if(new->top == new->capacity - 1)
+    {
+        printf("Stack is full\n");
         return;
     }
-    top--;
+    else
+        printf("Stack isn't full , it remains %d elements to be full\n", (new->capacity - new->top - 1));
 }
-void init(int *array)
+int main(void)
 {
-    
-    for(int i = 0, x ; i < n ; i++)
+    printf("How many elements in the stack : ");
+    do
     {
-        printf("Element %d : ", i);
-        scanf("%d", &x);
-        push(array, x);
-    }    
-}
-void print(int *array)
-{
-    for(int i = 0 ; i < top+1 ; i++)
-    {
-        printf("%d ", *(array+i));
-    }  
-    printf("\n");
+        scanf("%d", &n);
+    } while(n > 10 || n <= 0);
+    stack *first = create(n);
+    push(first, 5);
+    push(first, 3);
+    push(first, 5);
+    print(first);
+    is_full(first);
+
+    return 0;
 }
