ccc7dc475cde6bf0f1ef8c9a7354267f79c17eac
wassim31 <wassimbs088@gmail.com>
2021-11-19 01:58:22 +0100

    Queue Data-Structure :smile:

---
+++ Queues/main.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define SIZE 3
+typedef struct q q;
+struct q
+{
+    int rear, front;
+    unsigned size;
+    int *array;
+};
+void *create_q(unsigned size)
+{
+    q *new_q = malloc(sizeof(q));
+    new_q->front = -1;
+    new_q->rear = -1;
+    new_q->size = size;
+    new_q->array = malloc(sizeof(int) * size);
+}
+void enqueue(q *main, int value)
+{
+    if((main->rear +1) == main->size)
+    {
+        printf("Overflowed\n");
+        return;
+    }
+    if(main->rear == -1 && main->front == -1)
+    {
+        main->rear = 0;
+        main->front = 0;
+        // printf("test");
+    }
+    else
+    {
+        (main->rear)++;
+    }
+    main->array[main->rear] = value;
+}
+int dequeue(q *main)
+{
+    if(main->front  == main->size || main->front == -1)
+        return -1;
+    int value = main->array[main->front];
+    (main->front)++;
+    return value;
+}
+void print(q *main)
+{
+    for(int i = 0 ; i < SIZE ; i++)
+    {
+        printf("Element %d : %d\n", i, dequeue(main));
+    }
+
+}
+int main(int argc, char const *argv[])
+{
+    q *new_q = create_q(SIZE);
+    enqueue(new_q,1);
+    enqueue(new_q,2);
+    enqueue(new_q,3);
+    print(new_q);
+    return 0;
+}
